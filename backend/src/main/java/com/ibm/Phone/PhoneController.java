8a22581e3a742b3adf6ceb46f36aa093ef0833d6
Hemanth T S <hr000011@gmail.com>
2022-06-11 22:28:11 +0530

    update backend

--- backend/src/main/java/com/ibm/Phone/PhoneController.java
+++ backend/src/main/java/com/ibm/Phone/PhoneController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,37 +13,34 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ibm.Category.Category;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class PhoneController {
 
 	@Autowired
 	private PhoneService phoneService;
 
-	@GetMapping("/categories/{id}/phones")
-	public List<Phone> getAllPhones(@PathVariable String id) {
-		return phoneService.getAllPhones(id);
+	@GetMapping("/phones")
+	public List<Phone> getAllPhones() {
+		return phoneService.getAllPhones();
 	}
 
-	@GetMapping("/categories/{categoryId}/phones/{id}")
+	@GetMapping("/phones/{id}")
 	public Phone getPhone(@PathVariable String id) {
 		return phoneService.getPhone(id);
 	}
 
-	@PostMapping("/categories/{categoryId}/phones")
-	public void addPhone(@RequestBody Phone phone, @PathVariable String categoryId) {
-		phone.setTopic(new Category(categoryId,"",""));
+	@PostMapping("/phones")
+	public void addPhone(@RequestBody Phone phone) {
 		phoneService.addPhone(phone);
 	}
 
-	@PutMapping("/categories/{categoryId}/phones/{id}")
-	public void updatePhone(@RequestBody Phone phone, @PathVariable String categoryId) {
-		phone.setTopic(new Category(categoryId,"",""));
+	@PutMapping("/phones/{id}")
+	public void updatePhone(@RequestBody Phone phone) {
 		phoneService.updatePhone(phone);
 	}
 	
-	@DeleteMapping("/categories/{categoryId}/phones/{id}")
+	@DeleteMapping("/phones/{id}")
 	public void deletePhone(@PathVariable String id) {
 		phoneService.deletePhone(id);
 	}
--- backend/src/main/java/com/ibm/Phone/PhoneRepository.java
+++ backend/src/main/java/com/ibm/Phone/PhoneRepository.java
@@ -5,7 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PhoneRepository extends CrudRepository<Phone, String>{
-
-	public List<Phone> findByCategoryId(String categoryId);
 	
 }
